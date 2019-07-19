337dc726418f496921b4c39dfe8cd12b48c83546
Raul Velasco <raul93611@gmail.com>
2019-07-18 22:07:44 -0400

    I'm in video 11

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,52 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('roles', function(Blueprint $table){
+          $table-> bigIncrements('id');
+          $table-> string('name');
+          $table-> text('description');
+          $table-> timestamps();
+        });
+
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table-> unsignedBigInteger('role_id')-> default(\App\Role::STUDENT);
+            $table-> foreign('role_id')-> references('id')-> on('roles');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table-> string('picture')-> nullable();
+
+            $table-> string('stripe_id')-> nullable();
+            $table-> string('card_brand')-> nullable();
+            $table-> string('card_last_four')-> nullable();
+            $table-> timestamp('trial_ends_at')-> nullable();
+
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('subcriptions', function(Blueprint $table){
+          $table-> bigIncrements('id');
+          $table-> unsignedBigInteger('user_id');
+          $table-> foreign('user_id')-> references('id')-> on('users');
+          $table-> string('name');
+          $table-> string('stripe_id');
+          $table-> string('stripe_plan');
+          $table-> integer('quantity');
+          $table-> timestamp('trial_ends_at')-> nullable();
+          $table-> timestamp('ends_at')-> nullable();
+          $table-> timestamps();
+        });
+
+        Schema::create('user_social_accounts', function(Blueprint $table){
+          $table-> bigIncrements('id');
+          $table-> unsignedBigInteger('user_id');
+          $table-> foreign('user_id')-> references('id')-> on('users');
+          $table-> string('provider');
+          $table-> string('provider_uid');
         });
     }
 
@@ -31,6 +69,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+      Schema::dropIfExists('users');
+      Schema::dropIfExists('roles');
+      Schema::dropIfExists('subcriptions');
+      Schema::dropIfExists('user_social_accounts');
     }
 }
